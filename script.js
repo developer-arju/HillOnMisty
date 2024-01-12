31d56043777cfc9967e317b32abcd5234a9c1948
developer-arju <arjunsajeevan369@gmail.com>
2024-01-13 01:11:07 +0530

    Home, About, Amenities blocks completed

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-var toggleBtn = document.querySelector("#toggle-btn");
-var aside = document.querySelector("#side-menu");
+/** SMALL SCREEN SIDE MENU CLOSING  */
+document.addEventListener("scroll", () => {
+  const aside = document.querySelector("aside");
+  if (!aside) return;
 
-toggleBtn.addEventListener("click", toggleMenu);
+  if (aside.classList.contains("left-0")) {
+    aside.classList.replace("left-0", "-left-[75%]");
+  }
+});
 
-function toggleMenu(e) {
-  e.stopPropagation();
-  if (aside.classList.contains("-right-[250px]")) {
-    aside.classList.remove("-right-[250px]");
-    aside.classList.add("right-0");
+/** JUMP TO ELEMENTS WILL BE DELAYED ON SMALL SCREEN */
+function delayedScroll(event, targetId) {
+  event.preventDefault();
+  const delay = 500;
+
+  setTimeout(() => {
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+      toggleHamburger();
+    }
+  }, delay);
+}
+
+/** TOGGLE SIDE MENU IN SMALL SCREENS */
+function toggleHamburger() {
+  const asideMenu = document.querySelector("aside");
+
+  if (!asideMenu) return;
+
+  if (asideMenu.classList.contains("left-0")) {
+    asideMenu.classList.replace("left-0", "-left-[75%]");
   } else {
-    aside.classList.remove("right-0");
-    aside.classList.add("-right-[250px]");
+    asideMenu.classList.replace("-left-[75%]", "left-0");
   }
 }
